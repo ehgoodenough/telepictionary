c2624d2e73dad256c606566d31124b7ad8c6424b
Andrew McPherson <andrewmcp333@gmail.com>
2013-03-14 21:53:56 -0400

    Scripted the cursor to scribble on the canvas.

--- script.js
+++ script.js
@@ -4,13 +4,25 @@ window.addEventListener("load", function()
 {
 	canvas = document.getElementById("&");
 	context = canvas.getContext("2d");
+	
+	canvas.addEventListener("mousemove", continueScribbling, false);
+	
+	var sigmax = 0, sigmay = 0, thetax = 0, thetay = 0;
+	
+	function continueScribbling(event)
+	{
+		sigmax = thetax;
+		sigmay = thetay;
+		thetax = event.clientX;
+		thetay = event.clientY;
+		
+		context.strokeStyle = "#FA00AF";
+		context.lineCap = "round";
+		context.lineWidth = 5;
 
-	context.strokeStyle = "#FA00AF";
-	context.lineCap = "round";
-	context.lineWidth = 5;
-
-	context.beginPath();
-	context.moveTo(50, 100);
-	context.lineTo(100, 50);
-	context.stroke();
+		context.beginPath();
+		context.moveTo(sigmax, sigmay);
+		context.lineTo(thetax, thetay);
+		context.stroke();
+	}
 }, false);

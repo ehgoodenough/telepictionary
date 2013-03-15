1a30b46991a7b78711beedc8131dba661699dfb1
Andrew McPherson <andrewmcp333@gmail.com>
2013-03-15 17:44:36 -0400

    Added a cookie for alternating from text to image.

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+if(empty($_COOKIE['telepictionary']))
+{
+	setcookie( 'telepictionary', 'text', time() + 60, "/", "localhost");
+}
+?>
+
 <!DOCTYPE html><head><html><title>Telepictionary</title>
 <script type="text/javascript" src="script.js"></script>
 <link rel="stylesheet" type="text/css" href="style.css"/>

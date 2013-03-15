9cad551a4d8486c3349a231b2865645466f74527
Andrew McPherson <andrewmcp333@gmail.com>
2013-03-14 22:00:45 -0400

    Designated the scribbling to execute by clicking.

--- script.js
+++ script.js
@@ -5,24 +5,35 @@ window.addEventListener("load", function()
 	canvas = document.getElementById("&");
 	context = canvas.getContext("2d");
 	
+	canvas.addEventListener("mouseup", quitScribbling, false);
+	canvas.addEventListener("mouseout", quitScribbling, false);
+	canvas.addEventListener("mousedown", beginScribbling, false);
 	canvas.addEventListener("mousemove", continueScribbling, false);
 	
-	var sigmax = 0, sigmay = 0, thetax = 0, thetay = 0;
+	function beginScribbling(event) {continueScribbling(event); isScribbling = true;}
+	function quitScribbling(event) {continueScribbling(event); isScribbling = false;}
+	
+	var isScribbling = false;
+	var sigmax = 0, sigmay = 0;
+	var thetax = 0, thetay = 0;
 	
 	function continueScribbling(event)
 	{
-		sigmax = thetax;
-		sigmay = thetay;
-		thetax = event.clientX;
-		thetay = event.clientY;
-		
-		context.strokeStyle = "#FA00AF";
-		context.lineCap = "round";
-		context.lineWidth = 5;
+		if(isScribbling)
+		{
+			sigmax = thetax; sigmay = thetay;
+			
+			thetax = event.clientX;
+			thetay = event.clientY;
+			
+			context.strokeStyle = "#FA00AF";
+			context.lineCap = "round";
+			context.lineWidth = 5;
 
-		context.beginPath();
-		context.moveTo(sigmax, sigmay);
-		context.lineTo(thetax, thetay);
-		context.stroke();
+			context.beginPath();
+			context.moveTo(sigmax, sigmay);
+			context.lineTo(thetax, thetay);
+			context.stroke();
+		}
 	}
 }, false);

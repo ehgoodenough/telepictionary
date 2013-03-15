4e81580240b4340485e32f13810a1f1799b3b7e2
Andrew McPherson <andrewmcp333@gmail.com>
2013-03-14 21:13:08 -0400

    Initiated the project with boring blank files.

---
+++ index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html><head><html><title>Telepictionary</title>
+<script type="text/javascript" src="script.js"></script>
+<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+</body>
+</html>

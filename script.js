1e4a88c39e6ed58db2cb8e1071864097205753a1
Andrew McPherson <andrewmcp333@gmail.com>
2013-03-23 06:54:25 -0400

    Edited the code to acknowledge a scrolling offset.

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ function manageScribbling()
 	{
 		sigmaX = thetaX; sigmaY = thetaY;
 		
-		thetaX = event.clientX - canvas.offsetLeft - 1;
-		thetaY = event.clientY - canvas.offsetTop - 1;
+		thetaX = event.clientX - canvas.offsetLeft + window.pageXOffset - 1;
+		thetaY = event.clientY - canvas.offsetTop + window.pageYOffset - 1;
 		
 		if(isScribbling)
 		{

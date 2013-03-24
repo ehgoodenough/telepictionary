ab79a966c978177f7e6c4aa1fb97e63f2e894cbf
Andrew McPherson <andrewmcp333@gmail.com>
2013-03-24 18:34:19 -0400

    Recorded each entry into the database.

--- upload.php
+++ upload.php
@@ -1,14 +1,23 @@
 <?php
 
+$refnum = $_POST['refnum'];
+$source = $_POST['source'];
+$type = $_POST['type'];
+
 if($_POST['type'] == 'image')
 {
-	$source = $_POST['source'];
 	$source = substr($source, strpos($source, ",") + 1);
 	$source = base64_decode($source);
 	
 	file_put_contents('3242435.png', $source);
+	
+	$source = '3242435.png';
 }
 
-echo $_POST['refnum'] . " @ " . $_POST['type'] . " = " . $_POST['source'];
+$username = "root"; $password = ""; $dsn = "mysql:host=localhost;dbname=telepictionary";
+try {$db = new PDO($dsn, $username, $password);} catch(PDOException $e) {die($e->getMessage());}
+
+$insertion = $db->prepare("INSERT INTO telebits(refnum, source, type) VALUES(:refnum, :source, :type);");
+$insertion->execute(array(':refnum' => $refnum, 'source' => $source, ':type' => $type));
 
 ?>

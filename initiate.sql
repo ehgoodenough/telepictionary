42bd5a6e236b20ab4fbf89c39ad4812988403321
Andrew McPherson <andrewmcp333@gmail.com>
2013-03-27 12:36:14 -0400

    Again defined the default entries of the database.

--- initiate.sql
+++ initiate.sql
@@ -11,7 +11,11 @@ CREATE TABLE telebits
 ALTER TABLE telebits ADD PRIMARY KEY (idnum);
 ALTER TABLE telebits ADD FOREIGN KEY(refnum) REFERENCES telebits(idnum);
 
+INSERT INTO telebits(refnum, source, type) VALUES(1, "DFBFF", 'text');
+INSERT INTO telebits(refnum, source, type) VALUES(1, "Too much Pizza", 'text');
+INSERT INTO telebits(refnum, source, type) VALUES(1, "I am a bit confused", 'text');
 INSERT INTO telebits(refnum, source, type) VALUES(1, "The graves are on fire!", 'text');
-INSERT INTO telebits(refnum, source, type) VALUES(1, "20130326004440.png", 'image');
-INSERT INTO telebits(refnum, source, type) VALUES(1, "20130326004456.png", 'image');
-INSERT INTO telebits(refnum, source, type) VALUES(2, "DFBFF", 'text');
+INSERT INTO telebits(refnum, source, type) VALUES(1, "Upside Down Zombie", 'text');
+INSERT INTO telebits(refnum, source, type) VALUES(1, "Stich in Time Saves Nine", 'text');
+INSERT INTO telebits(refnum, source, type) VALUES(1, "Who has seen the wind?", 'text');
+INSERT INTO telebits(refnum, source, type) VALUES(1, "Rocket to Mars", 'text');
